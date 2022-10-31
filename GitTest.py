a0f81152d8f9182bdb81c9b034e60c38f726df90
Benjamin <109101722+blawrence5298@users.noreply.github.com>
2022-10-31 14:11:47 +0000

    added confirmation to gitTest

---
+++ GitTest.py
@@ -0,0 +1,3 @@
+def message():
+  print("Let's see if the commit works")
+  print("Yest the commit worked")
--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import Project1
 import Project2
 import Project3
+import GitTest
+
 
 def main():
 
   projectNum = eval(
-  input("Please enter the project number whose output you wish to view: "))
+    input("Please enter the project number whose output you wish to view: "))
   print("")
 
   if projectNum == 1:
     age = int(input("Hello! Please enter your age:"))
     Project1.votingAge(age)
-    
+
   elif projectNum == 2:
     three_num = (input("Please list three numbers separated by a comma:"))
     max_list = [int(x) for x in three_num.split(",")]
-    max = Project2.maxThree(max_list[0],max_list[1],max_list[2])
+    max = Project2.maxThree(max_list[0], max_list[1], max_list[2])
     print(max)
     return
-  
+
   elif projectNum == 3:
     Project3.numberGame()
     return
-    
+
+  elif projectNum == 4:
+    GitTest.message()
+    return
+
   else:
     print("Sorry, that is not a valid project number")
-    
+
+
 main()
